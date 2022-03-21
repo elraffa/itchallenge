341edfa9f468f03129e8d83ca99b14c0bb095612
elraffa <federicoruffa@gmail.com>
2022-03-21 13:47:43 -0300

    serve build folder

--- server.js
+++ server.js
@@ -31,10 +31,10 @@ if (process.env.NODE_ENV === 'production') {
   //app.use(express.static('client/build'));
   app.use(express.static(path.resolve(__dirname, './client/build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
-
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 // // Avoid 304 Not Modified
 // app.get('/*', function (req, res, next) {
 //   res.setHeader('Last-Modified', new Date().toUTCString());
