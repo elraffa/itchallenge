6d169a9009891f0a09c423477dc4ce41f7966ce6
elraffa <federicoruffa@gmail.com>
2022-03-21 13:39:10 -0300

    change fetch endpoint

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ app.use('/api/products', require('./routes/products'));
 // app.use(express.static(path.resolve(__dirname, './client/build')));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  //app.use(express.static('client/build'));
-  app.use(express.static(path.resolve(__dirname, './client/build')));
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   //app.use(express.static('client/build'));
+//   app.use(express.static(path.resolve(__dirname, './client/build')));
 
   // app.get('*', (req, res) => {
   //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
