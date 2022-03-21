6f2caafaf204ccc38876c0a1a2bde9a83082e087
elraffa <federicoruffa@gmail.com>
2022-03-21 11:15:39 -0300

    push to heroku

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 var cors = require('cors');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
 // Init middleware
 app.use(express.json({ extended: false }));
+
+//This will create a middleware.
+//When you navigate to the root page, it would use the built react-app
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
 // Connect database
 connectDB();
